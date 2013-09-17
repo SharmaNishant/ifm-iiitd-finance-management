ce51e10f5e3ae57a43beef9da023e354a1b5021c
nishant11070 <nishant11070@iiitd.ac.in>
2013-09-17 23:35:03 +0530

    added login functionally

--- src/java/servlet/controller/login.java
+++ src/java/servlet/controller/login.java
@@ -11,78 +11,34 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 /**
  *
  * @author Nishant
  */
 public class login extends HttpServlet {
+    
 
-    /**
-     * Processes requests for both HTTP
-     * <code>GET</code> and
-     * <code>POST</code> methods.
-     *
-     * @param request servlet request 
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        try {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet login</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet login at " + request.getContextPath() + "</h1>");
-            //out.println("</body>");
-            //out.println("</html>");
-        } finally {            
-            //out.close();
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP
-     * <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        
     }
 
-    /**
-     * Handles the HTTP
-     * <code>POST</code> method.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
             response.setContentType("text/html;charset=UTF-8");
-            PrintWriter out1 = response.getWriter();
-            processRequest(request, response);
             String mail = request.getParameter("email");
             String pass = request.getParameter("pass");
-            out1.println("<h2>"+"Hello to </h2> <h3>"+mail+"</h3> <h2>  by Nishant"+"</h2>"+"</BR>"+"<p>"+"Site is Under Development. Please visit again after some time.</p></BR><h3>"+"Thanks For Visiting"+"</h3>");
-            out1.println("</body>");
-            out1.println("</html>");
-            out1.close();
+            String result = servlet.model.login.getPassword(mail);
+            //response.addCookie(null);
+            if(pass.equals(result))
+            {
+                request.setAttribute("mail", mail);
+                request.getRequestDispatcher("/IFM/dist/profile.jsp").forward(request, response);
+            }
             
     }
 
