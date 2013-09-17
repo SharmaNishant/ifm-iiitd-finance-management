07f679f2898c6fccb2cf1da592b4eb071faa6883
rishav11088 <rishav11088@localhost>
2013-09-17 21:44:11 +0530

    added the mysql connector, made function to search for password

--- src/java/servlet/model/login.java
+++ src/java/servlet/model/login.java
@@ -5,7 +5,8 @@
 package servlet.model;
 
 import java.sql.*;
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -25,6 +26,43 @@ public class login {
         catch (Exception e) {  
             e.printStackTrace();  
         }  
-    }  
+    }
     
+    /**
+     * This function searches for password corresponding to email address.
+     * 
+     * @param email
+     * @return password
+     */
+    public String getPassword(String email) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ifm", "root", "admin");
+            
+            Statement smt = conn.createStatement();
+                                    
+            ResultSet rs = smt.executeQuery("SELECT * FROM PASSWORD WHERE EMAIL=\"" + email + "\"");
+
+            String ret = null;
+            
+            while(rs.next()) {
+                ret = rs.getString("Pass");
+            }
+            smt.close();
+            
+            return ret;
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(login.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(login.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    /*  FOR TESTING FUNCTIONS IN THIS CLASS ONLY
+     * 
+     
+    public static void main(String[] args) {
+        System.out.println(getPassword("rishav11088@iiitd.ac.in"));
+    }
+    * /
 }
