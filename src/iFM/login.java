fb32b62e17f9ae11f488827ea83eae5be4992ddf
nishant11070 <nishant11070@iiitd.ac.in>
2013-10-06 19:58:30 +0530

    Backed out merge changeset: fee7b5632f6c
    Backed out merge revision to its first parent (31d2ca447fce)

--- src/iFM/login.java
+++ src/iFM/login.java
@@ -4,42 +4,26 @@
  */
 package iFM;
 
-import iFM.Model.Users;
-
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.google.appengine.api.datastore.Entity;
-
+    
 /**
  *
  * @author Greg Bakos <greg@londonfreelancers.co.uk>
  */
 @SuppressWarnings("serial")
 public class login extends HttpServlet {
-	
-	
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
   
     		String a[]={"1","1","1","1","1","1","1","1","1"};
     		request.setAttribute("profile", a);
-    		
-    		Entity user = Users.getProfile("rishav11088@iiitd.ac.in");
-    		
-    		if( user != null) {
-    			System.out.println(user.getProperty("Name"));
-    		
-    			request.getRequestDispatcher("profile.jsp").forward(request, response);
-    		}
-    		else {
-    			System.out.println("User Not Found");
-    		}
+            request.getRequestDispatcher("profile.jsp").forward(request, response);
+
     }
 
 
--- src/iFM/test.java
+++ src/iFM/test.java
@@ -1,7 +1,5 @@
 package iFM;
 
-import iFM.Model.Users;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -14,18 +12,12 @@ import javax.servlet.http.HttpServletResponse;
 
 @SuppressWarnings("serial")
 public class test extends HttpServlet {
-	
-	List<String> test=new ArrayList<String>();  
+    List<String> test=new ArrayList<String>();  
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-    	Users.addUser("Rishav Jain", "rishav11088@iiitd.ac.in", "rishav", "BT2011088", "", "", "", "");
-    	Users.addUser("Nishant Sharma", "nishant11070@iiitd.ac.in", "nishant", "BT2011070", "", "", "", "");
-    	Users.addUser("Mayank Garg", "mayank11065@iiitd.ac.in", "mayank", "BT2011065", "", "", "", "");
-    	
-    	test.add("Sample Users Added");
-    	
-    	response.setContentType("text/html;charset=UTF-8");
+    	test.add("test string");//your function Rishav 
+    	 response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         try {
             out.println("<!DOCTYPE html>");
