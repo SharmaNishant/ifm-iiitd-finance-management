60ef924e2e6353cac41f544e11ad2576d79dc8e1
rishav11088 <rishav11088@localhost>
2013-10-06 15:44:37 +0530

    send verification mail functionality added

--- src/iFM/Model/Users.java
+++ src/iFM/Model/Users.java
@@ -3,7 +3,16 @@
  */
 package iFM.Model;
 
+import java.io.UnsupportedEncodingException;
 import java.util.Date;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
@@ -22,6 +31,33 @@ public class Users {
 		return now.getTime() + roll;
 	}
 	
+	private static void sendEmail(String email, String token) {
+		String messageBody =
+				"Welcome to iFM, Your account has been created.\n\n" +
+				"You can confirm your account by clicking on the following link -\n" +
+				"http://1.iiitdfm.appspot.com/NewUser.jsp?uid=" + token + "\n\n" +
+				"The iFM Team\n";
+		
+		Properties props = new Properties(); 
+        Session session = Session.getDefaultInstance(props, null); 
+		Message message = new MimeMessage(session);
+		
+		try {
+			message.setFrom(new InternetAddress("no-reply@iiitdfm.appspotmail.com", "iFM TEAM"));
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(email));
+			message.setSubject("Welcome to iFM");
+			message.setText(messageBody);
+			Transport.send(message);
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
 	public static void addUser(String name, String email, String password, String instituteid, String phone, String address, String role, String designation) {
 		
 		String token = generateToken(instituteid);
@@ -37,6 +73,8 @@ public class Users {
 		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 		datastore.put(userEntity);
 		datastore.put(accountEntity);
+		
+		sendEmail(email, token);
 	}
 	
 	public static Entity getProfile(String email) {
